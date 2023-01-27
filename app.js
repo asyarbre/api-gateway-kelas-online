1541e11920f13721eda5abc0a1b3f36e7a91c10b
Asya'ri ‎ <asyari818@gmail.com>
2023-01-27 11:27:52 +0700

    Setup route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
 
 const app = express();
 
@@ -17,5 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
 
 module.exports = app;
