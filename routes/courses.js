af48b1e9c2a6c6ff1787ef8d93a17dc448cdc258
Asya'ri ‎ <asyari818@gmail.com>
2023-02-06 14:04:55 +0700

    [Courses] - API Get Course by ID

--- routes/courses.js
+++ routes/courses.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const coursesHandler = require('./handler/courses');
 
 router.get('/', coursesHandler.getAll);
+router.get('/:id', coursesHandler.get);
  
  module.exports = router;
