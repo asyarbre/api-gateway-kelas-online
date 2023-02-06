bb1d76ebb401f12468a80eaedcfd7ebca0370459
Asya'ri ‎ <asyari818@gmail.com>
2023-02-06 13:55:55 +0700

    [Courses] - API Get List Courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const coursesHandler = require('./handler/courses');
 
-module.exports = router;
+router.get('/', coursesHandler.getAll);
+ 
+ module.exports = router;
