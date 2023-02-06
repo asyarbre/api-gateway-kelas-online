a64b2ffb409d5be5681f407d11652846aee3549e
Asya'ri ‎ <asyari818@gmail.com>
2023-02-06 15:24:11 +0700

    [Chapters] - API Get All & by ID, Add Middleware

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,5 +6,7 @@ const chaptersHandler = require('./handler/chapters');
 router.post('/', chaptersHandler.create);
 router.put('/:id', chaptersHandler.update);
 router.delete('/:id', chaptersHandler.destroy);
+router.get('/', chaptersHandler.getAll);
+router.get('/:id', chaptersHandler.get);
 
 module.exports = router;
