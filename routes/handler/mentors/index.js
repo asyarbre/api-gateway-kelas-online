846f2d07a3e5f90eef2201e1a6219cf1ab91068a
Asya'ri ‎ <asyari818@gmail.com>
2023-02-06 11:05:04 +0700

    [Mentors] - API Update

--- routes/handler/mentors/index.js
+++ routes/handler/mentors/index.js
@@ -1,9 +1,11 @@
 const getAll = require('./getAll');
 const get = require('./get');
 const create = require('./create');
+const update = require('./update');
 
 module.exports = {
   getAll,
   get,
   create,
+  update,
 };
