74a87763cd0eafae4d9e6dc9ef760a06a30c87dc
Asya'ri ‎ <asyari818@gmail.com>
2023-02-06 14:11:20 +0700

    [Courses] - API Create

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const coursesHandler = require('./handler/courses');
 
+const verifyToken = require('../middlewares/verifyToken');
+
 router.get('/', coursesHandler.getAll);
 router.get('/:id', coursesHandler.get);
+
+router.post('/', coursesHandler.create);
  
  module.exports = router;
