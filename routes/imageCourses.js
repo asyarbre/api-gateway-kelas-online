af41cd51354e25bf5593d8b548f2cc6a4cf4858c
Asya'ri ‎ <asyari818@gmail.com>
2023-02-07 08:47:28 +0700

    [Image Course] - API Delete

--- routes/imageCourses.js
+++ routes/imageCourses.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const imageCourseHandler = require('./handler/image-courses');
 
 router.post('/', imageCourseHandler.create);
+router.delete('/:id', imageCourseHandler.destroy);
 
 module.exports = router;
